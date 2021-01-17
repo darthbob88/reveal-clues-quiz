d6884c26581b8daa57627971e6e5ff77ce3525c1
Raymond Price <darthbob88@users.noreply.github.com>
2021-01-16 16:11:38 -0800

    Removed the `cleanup` import as unnecessary and blocking the build

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,4 +1,3 @@
-import { cleanup } from "@testing-library/react";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Quiz } from "../../model/Quiz";
