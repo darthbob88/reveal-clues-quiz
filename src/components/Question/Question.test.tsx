83c72f53ec789d1eb18dbb7703cb87bd74a66009
Raymond Price <darthbob88@users.noreply.github.com>
2021-01-30 15:43:14 -0800

    Adding tests to properly render answered questions

--- src/components/Question/Question.test.tsx
+++ src/components/Question/Question.test.tsx
@@ -12,143 +12,195 @@ const defaultQuestion = defaultQuiz.questions[0];
 const awardPoints = (score: number) => {
   console.log(score);
 };
-test("renders a question properly", () => {
-  const { container, getByText, getAllByText } = render(
-    <QuestionComponent
-      state={defaultQuestionState}
-      question={defaultQuestion}
-      awardPoints={awardPoints}
-      prompt={defaultQuiz.prompt}
-    />
-  );
-  const firstClue = getByText(defaultQuestion.clues[0]);
-  expect(firstClue).toBeInTheDocument();
-  const unrevealedClues = getAllByText("-----------");
-  expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 1);
+describe("Question component", () => {
+  test("renders a question properly", () => {
+    const { container, getByText, getAllByText } = render(
+      <QuestionComponent
+        state={defaultQuestionState}
+        question={defaultQuestion}
+        awardPoints={awardPoints}
+        prompt={defaultQuiz.prompt}
+      />
+    );
+    const firstClue = getByText(defaultQuestion.clues[0]);
+    expect(firstClue).toBeInTheDocument();
+    const unrevealedClues = getAllByText("-----------");
+    expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 1);
 
-  const revealClue = getByText("Reveal Another Clue");
-  expect(revealClue).toBeEnabled();
+    const revealClue = getByText("Reveal Another Clue");
+    expect(revealClue).toBeEnabled();
 
-  expect(container).toMatchSnapshot();
-});
-// Can't easily test this after moving `revealedClues` to MobX :(
-xtest("reveals more clues as necessary", () => {
-  const { queryByText, getByText, getAllByText } = render(
-    <QuestionComponent
-      state={defaultQuestionState}
-      question={defaultQuestion}
-      awardPoints={awardPoints}
-    />
-  );
-  const revealClue = getByText("Reveal Another Clue");
-  expect(revealClue).toBeEnabled();
+    expect(container).toMatchSnapshot();
+  });
 
-  // Should only show one clue at first
-  const firstClue = getByText(defaultQuestion.clues[0]);
-  expect(firstClue).toBeInTheDocument();
-  let unrevealedClues = getAllByText("-----------");
-  expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 1);
+  test("renders a correctly answered question properly", () => {
+    const modifiedState = { ...defaultQuestionState, state: QuestionEnum.CORRECTLY_ANSWERED };
+    const { container, getByText, getAllByText } = render(
+      <QuestionComponent
+        state={modifiedState}
+        question={defaultQuestion}
+        awardPoints={awardPoints}
+        prompt={defaultQuiz.prompt}
+      />
+    );
+    const firstClue = getByText(defaultQuestion.clues[0]);
+    expect(firstClue).toBeInTheDocument();
+    const unrevealedClues = getAllByText("-----------");
+    expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 1);
 
-  // Should reveal a second clue
-  fireEvent.click(revealClue);
-  const secondClue = getByText(defaultQuestion.clues[1]);
-  expect(secondClue).toBeInTheDocument();
-  unrevealedClues = getAllByText("-----------");
-  expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 2);
+    const revealClue = getByText("Reveal Another Clue");
+    expect(revealClue).toBeDisabled();
 
-  // Should reveal a third clue
-  fireEvent.click(revealClue);
-  const thirdClue = getByText(defaultQuestion.clues[2]);
-  expect(thirdClue).toBeInTheDocument();
-  unrevealedClues = getAllByText("-----------");
-  expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 3);
+    // TODO: How can I check that it's awarding the correct number of points?
+    const correctAnswer = getByText(/Correct/i);
+    expect(correctAnswer).toBeInTheDocument();
 
-  // Should reveal all four clues
-  fireEvent.click(revealClue);
-  const fourthClue = getByText(defaultQuestion.clues[3]);
-  expect(fourthClue).toBeInTheDocument();
-  expect(queryByText("-----------")).not.toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
 
-  // After we reveal all four clues, the "Reveal Clue" button should be disabled
-  expect(revealClue).toBeDisabled();
-});
+  test("renders an incorrectly answered question properly", () => {
+    const modifiedState = { ...defaultQuestionState, state: QuestionEnum.INCORRECTLY_ANSWERED };
+    const { container, getByText, getAllByText } = render(
+      <QuestionComponent
+        state={modifiedState}
+        question={defaultQuestion}
+        awardPoints={awardPoints}
+        prompt={defaultQuiz.prompt}
+      />
+    );
+    const firstClue = getByText(defaultQuestion.clues[0]);
+    expect(firstClue).toBeInTheDocument();
+    const unrevealedClues = getAllByText("-----------");
+    expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 1);
 
-test("awards 4 points for correct answer with 1 clue", () => {
-  const incrementScore = jest.fn();
-  const { getByText, getByLabelText } = render(
-    <QuestionComponent
-      state={defaultQuestionState}
-      question={defaultQuestion}
-      awardPoints={incrementScore}
-      prompt={defaultQuiz.prompt}
-    />
-  );
-  const answerSlot = getByLabelText(/Answer/i);
-  fireEvent.change(answerSlot, { target: { value: defaultQuestion.answer } });
+    const revealClue = getByText("Reveal Another Clue");
+    expect(revealClue).toBeDisabled();
 
-  const submitBtn = getByText(/Submit/i);
-  fireEvent.click(submitBtn);
+    const incorrectAnswer = getByText(/Incorrect/i);
+    expect(incorrectAnswer).toBeInTheDocument();
 
-  const expectedState: QuestionState = {
-    state: QuestionEnum.CORRECTLY_ANSWERED,
-    score: defaultQuestion.clues.length,
-    revealedClues: 0,
-  };
-  expect(incrementScore).toHaveBeenCalledTimes(1);
-  expect(incrementScore).toHaveBeenCalledWith(expectedState);
-});
+    expect(container).toMatchSnapshot();
+  });
 
-test("awards 3 points for correct answer with 2 clues", () => {
-  const incrementScore = jest.fn();
-  const { getByText, getByLabelText } = render(
-    <QuestionComponent
-      state={defaultQuestionState}
-      question={defaultQuestion}
-      awardPoints={incrementScore}
-      prompt={defaultQuiz.prompt}
-    />
-  );
+  // Can't easily test this after moving `revealedClues` to MobX :(
+  xtest("reveals more clues as necessary", () => {
+    const { queryByText, getByText, getAllByText } = render(
+      <QuestionComponent
+        state={defaultQuestionState}
+        question={defaultQuestion}
+        awardPoints={awardPoints}
+      />
+    );
+    const revealClue = getByText("Reveal Another Clue");
+    expect(revealClue).toBeEnabled();
 
-  const revealClue = getByText("Reveal Another Clue");
-  expect(revealClue).toBeEnabled();
-  fireEvent.click(revealClue);
+    // Should only show one clue at first
+    const firstClue = getByText(defaultQuestion.clues[0]);
+    expect(firstClue).toBeInTheDocument();
+    let unrevealedClues = getAllByText("-----------");
+    expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 1);
 
-  const answerSlot = getByLabelText(/Answer/i);
-  fireEvent.change(answerSlot, { target: { value: defaultQuestion.answer } });
+    // Should reveal a second clue
+    fireEvent.click(revealClue);
+    const secondClue = getByText(defaultQuestion.clues[1]);
+    expect(secondClue).toBeInTheDocument();
+    unrevealedClues = getAllByText("-----------");
+    expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 2);
 
-  const submitBtn = getByText(/Submit/i);
-  fireEvent.click(submitBtn);
+    // Should reveal a third clue
+    fireEvent.click(revealClue);
+    const thirdClue = getByText(defaultQuestion.clues[2]);
+    expect(thirdClue).toBeInTheDocument();
+    unrevealedClues = getAllByText("-----------");
+    expect(unrevealedClues.length).toBe(defaultQuestion.clues.length - 3);
 
-  const expectedState: QuestionState = {
-    state: QuestionEnum.CORRECTLY_ANSWERED,
-    score: defaultQuestion.clues.length - 1,
-    revealedClues: 1,
-  };
-  expect(incrementScore).toHaveBeenCalledTimes(1);
-  expect(incrementScore).toHaveBeenCalledWith(expectedState);
-});
+    // Should reveal all four clues
+    fireEvent.click(revealClue);
+    const fourthClue = getByText(defaultQuestion.clues[3]);
+    expect(fourthClue).toBeInTheDocument();
+    expect(queryByText("-----------")).not.toBeInTheDocument();
 
-test("awards 0 points for incorrect answer", () => {
-  const incrementScore = jest.fn();
-  const { getByText, getByLabelText } = render(
-    <QuestionComponent
-      state={defaultQuestionState}
-      question={defaultQuestion}
-      awardPoints={incrementScore}
-      prompt={defaultQuiz.prompt}
-    />
-  );
-  const answerSlot = getByLabelText(/Answer/i);
-  fireEvent.change(answerSlot, { target: { value: "butts" } });
+    // After we reveal all four clues, the "Reveal Clue" button should be disabled
+    expect(revealClue).toBeDisabled();
+  });
 
-  const submitBtn = getByText(/Submit/i);
-  fireEvent.click(submitBtn);
+  test("awards 4 points for correct answer with 1 clue", () => {
+    const incrementScore = jest.fn();
+    const { getByText, getByLabelText } = render(
+      <QuestionComponent
+        state={defaultQuestionState}
+        question={defaultQuestion}
+        awardPoints={incrementScore}
+        prompt={defaultQuiz.prompt}
+      />
+    );
+    const answerSlot = getByLabelText(/Answer/i);
+    fireEvent.change(answerSlot, { target: { value: defaultQuestion.answer } });
 
-  const expectedState: QuestionState = {
-    state: QuestionEnum.INCORRECTLY_ANSWERED,
-    score: 0,
-    revealedClues: 1,
-  };
-  expect(incrementScore).toHaveBeenCalledTimes(1);
-  expect(incrementScore).toHaveBeenCalledWith(expectedState);
-});
+    const submitBtn = getByText(/Submit/i);
+    fireEvent.click(submitBtn);
+
+    const expectedState: QuestionState = {
+      state: QuestionEnum.CORRECTLY_ANSWERED,
+      score: defaultQuestion.clues.length,
+      revealedClues: 0,
+    };
+    expect(incrementScore).toHaveBeenCalledTimes(1);
+    expect(incrementScore).toHaveBeenCalledWith(expectedState);
+  });
+
+  test("awards 3 points for correct answer with 2 clues", () => {
+    const incrementScore = jest.fn();
+    const { getByText, getByLabelText } = render(
+      <QuestionComponent
+        state={defaultQuestionState}
+        question={defaultQuestion}
+        awardPoints={incrementScore}
+        prompt={defaultQuiz.prompt}
+      />
+    );
+
+    const revealClue = getByText("Reveal Another Clue");
+    expect(revealClue).toBeEnabled();
+    fireEvent.click(revealClue);
+
+    const answerSlot = getByLabelText(/Answer/i);
+    fireEvent.change(answerSlot, { target: { value: defaultQuestion.answer } });
+
+    const submitBtn = getByText(/Submit/i);
+    fireEvent.click(submitBtn);
+
+    const expectedState: QuestionState = {
+      state: QuestionEnum.CORRECTLY_ANSWERED,
+      score: defaultQuestion.clues.length - 1,
+      revealedClues: 1,
+    };
+    expect(incrementScore).toHaveBeenCalledTimes(1);
+    expect(incrementScore).toHaveBeenCalledWith(expectedState);
+  });
+
+  test("awards 0 points for incorrect answer", () => {
+    const incrementScore = jest.fn();
+    const { getByText, getByLabelText } = render(
+      <QuestionComponent
+        state={defaultQuestionState}
+        question={defaultQuestion}
+        awardPoints={incrementScore}
+        prompt={defaultQuiz.prompt}
+      />
+    );
+    const answerSlot = getByLabelText(/Answer/i);
+    fireEvent.change(answerSlot, { target: { value: "butts" } });
+
+    const submitBtn = getByText(/Submit/i);
+    fireEvent.click(submitBtn);
+
+    const expectedState: QuestionState = {
+      state: QuestionEnum.INCORRECTLY_ANSWERED,
+      score: 0,
+      revealedClues: 1,
+    };
+    expect(incrementScore).toHaveBeenCalledTimes(1);
+    expect(incrementScore).toHaveBeenCalledWith(expectedState);
+  });
+})
