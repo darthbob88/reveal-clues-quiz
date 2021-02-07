da74977b305486cc973dee35c87c2bbdda4b1157
Raymond Price <darthbob88@users.noreply.github.com>
2021-02-06 23:06:06 -0800

    Shuffling the questions for replay value.

--- src/components/Quiz/QuizPage.tsx
+++ src/components/Quiz/QuizPage.tsx
@@ -14,10 +14,37 @@ export const QuizPage: React.FunctionComponent = () => {
     useEffect(() => {
         const fetchData = async (id: string) => {
             const result = await loadQuiz(id);
+
+            // Shuffling the Questions array here instead of the component
+            // to maintain consistency for testing.
+            const questions = result.questions;
+            shuffle(questions);
+            result.questions = questions;
+
             setQuiz(result);
         };
         id != null && fetchData(id);
     }, [id]);
+
+    function shuffle<T>(array: T[]): T[] {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
     return (
         quiz === undefined ? <h2>Still Loading</h2> : <QuizComponent quiz={new QuizState(quiz)} />
     );
