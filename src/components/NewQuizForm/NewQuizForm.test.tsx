d8a5f50c7ceed9739b0aa968d4b21649992695a9
Raymond Price (INSIGHT GLOBAL) <v-raprice@microsoft.com>
2023-06-26 12:31:38 -0700

    Grouping New Quiz tests into sub-suites

--- src/components/NewQuizForm/NewQuizForm.test.tsx
+++ src/components/NewQuizForm/NewQuizForm.test.tsx
@@ -10,81 +10,91 @@ describe("New Quiz component", () => {
 
     expect(container).toMatchSnapshot();
   });
+  describe("Properly handles adding/removing clues/questions", () => {
+    test("adds a new question when the button is clicked", () => {
+      const { getByText, queryByText } = render(
+        <NewQuizForm />
+      );
 
-  test("adds a new question when the button is clicked", () => {
-    const { getByText, queryByText } = render(
-      <NewQuizForm />
-    );
+      let result = queryByText(/Question 2/i);
+      expect(result).not.toBeInTheDocument();
 
-    let result = queryByText(/Question 2/i);
-    expect(result).not.toBeInTheDocument();
+      const addQBtn = getByText(/Add Question/i);
+      fireEvent.click(addQBtn);
 
-    const addQBtn = getByText(/Add Question/i);
-    fireEvent.click(addQBtn);
+      result = queryByText(/Question 2/i);
+      expect(result).toBeInTheDocument();
+    });
 
-    result = queryByText(/Question 2/i);
-    expect(result).toBeInTheDocument();
+    test("removes a clue from a question when the button is clicked", () => {
+      const { getByTestId } = render(
+        <NewQuizForm />
+      );
+
+      const question = getByTestId("question1");
+      expect(question).toBeInTheDocument();
+
+      const clue1 = within(question).queryByTestId("clue1");
+      expect(clue1).toBeInTheDocument();
+
+      let clue4 = within(question).queryByTestId("clue4");
+      expect(clue4).toBeInTheDocument();
+
+      const removeClueQBtn = within(question).getAllByTestId("remove-clue");
+      expect(removeClueQBtn).toHaveLength(4);
+      fireEvent.click(removeClueQBtn[0]);
+
+      clue4 = within(question).queryByTestId("clue4");
+      expect(clue4).not.toBeInTheDocument();
+    });
+
+    test("removes a question when the button is clicked", () => {
+      const { getByText, getByTestId, queryByTestId } = render(
+        <NewQuizForm />
+      );
+
+      const addQBtn = getByText(/Add Question/i);
+      fireEvent.click(addQBtn);
+
+      const question = getByTestId("question2");
+      expect(question).toBeInTheDocument();
+
+      const removeQBtn = within(question).getByTestId("remove-question");
+      fireEvent.click(removeQBtn);
+
+      const question2 = queryByTestId("question2");
+      expect(question2).not.toBeInTheDocument();
+    });
+
+    test("adds a clue to a question when the button is clicked", () => {
+      const { getByTestId } = render(
+        <NewQuizForm />
+      );
+
+      const question = getByTestId("question1");
+      expect(question).toBeInTheDocument();
+
+      const clue1 = within(question).queryByTestId("clue1");
+      expect(clue1).toBeInTheDocument();
+
+      let clue5 = within(question).queryByTestId("clue5");
+      expect(clue5).not.toBeInTheDocument();
+
+      const addClueQBtn = within(question).getByTestId("add-clue");
+      fireEvent.click(addClueQBtn);
+
+      clue5 = within(question).queryByTestId("clue5");
+      expect(clue5).toBeInTheDocument();
+    });
   });
 
-  test("removes a clue from a question when the button is clicked", () => {
-    const { getByTestId } = render(
-      <NewQuizForm />
-    );
+  describe.skip("Properly handles submitting a quiz", () => {
 
-    const question = getByTestId("question1");
-    expect(question).toBeInTheDocument();
-
-    const clue1 = within(question).queryByTestId("clue1");
-    expect(clue1).toBeInTheDocument();
-
-    let clue4 = within(question).queryByTestId("clue4");
-    expect(clue4).toBeInTheDocument();
-
-    const removeClueQBtn = within(question).getAllByTestId("remove-clue");
-    expect(removeClueQBtn).toHaveLength(4);
-    fireEvent.click(removeClueQBtn[0]);
-
-    clue4 = within(question).queryByTestId("clue4");
-    expect(clue4).not.toBeInTheDocument();
+    // Input a test quiz and make sure it gets submitted correctly.
+    test("Can handle submitting a good quiz", () => {
+      const { getByText, queryByText } = render(
+        <NewQuizForm />
+      );
+    });
   });
-
-  test("removes a question when the button is clicked", () => {
-    const { getByText, getByTestId, queryByTestId } = render(
-      <NewQuizForm />
-    );
-
-    const addQBtn = getByText(/Add Question/i);
-    fireEvent.click(addQBtn);
-
-    const question = getByTestId("question2");
-    expect(question).toBeInTheDocument();
-
-    const removeQBtn = within(question).getByTestId("remove-question");
-    fireEvent.click(removeQBtn);
-
-    const question2 = queryByTestId("question2");
-    expect(question2).not.toBeInTheDocument();
-  });
-
-  test("adds a clue to a question when the button is clicked", () => {
-    const { getByTestId } = render(
-      <NewQuizForm />
-    );
-
-    const question = getByTestId("question1");
-    expect(question).toBeInTheDocument();
-
-    const clue1 = within(question).queryByTestId("clue1");
-    expect(clue1).toBeInTheDocument();
-
-    let clue5 = within(question).queryByTestId("clue5");
-    expect(clue5).not.toBeInTheDocument();
-
-    const addClueQBtn = within(question).getByTestId("add-clue");
-    fireEvent.click(addClueQBtn);
-
-    clue5 = within(question).queryByTestId("clue5");
-    expect(clue5).toBeInTheDocument();
-  });
-
-})
+});
