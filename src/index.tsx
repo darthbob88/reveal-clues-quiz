cc05a4d22d50d33d25acefb232f6e911fe2b9738
Raymond Price <darthbob88@users.noreply.github.com>
2023-07-28 22:12:08 -0700

    Moving Firebase to separate file.

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
-import { initializeApp } from "firebase/app";
-
-initializeApp({
-  apiKey: "AIzaSyBjboMH-rwsiD1PsG-LrlQWx-6-Fa_p96M",
-  authDomain: "reveal-clues-quiz.firebaseapp.com",
-  projectId: "reveal-clues-quiz",
-  storageBucket: "reveal-clues-quiz.appspot.com",
-  messagingSenderId: "74030913512",
-  appId: "1:74030913512:web:91fdf6941e3ce3abf0365d"
-});
 
 ReactDOM.render(
   <React.StrictMode>
