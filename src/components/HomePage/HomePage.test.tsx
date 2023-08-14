864f4d4f08611f1288a23287537e1c363ca133f2
Raymond Price <darthbob88@users.noreply.github.com>
2023-08-14 00:16:19 -0700

    Fixing tests

--- src/components/HomePage/HomePage.test.tsx
+++ src/components/HomePage/HomePage.test.tsx
@@ -3,35 +3,32 @@ import { act, render, waitFor, waitForElementToBeRemoved } from "@testing-librar
 import { HomePage } from "./HomePage";
 import { MemoryRouter } from "react-router-dom";
 import { testQuizzes } from "../../model/Quiz";
+import { loadAllQuizzes } from "../../model/QuizService";
+
+jest.createMockFromModule("../../model/QuizService");
 
 // TODO: For some reason, these tests fail because of Firebase. Fix that, would you?
-xdescribe("Home page", () => {
+describe("Home page", () => {
 
   test("renders the home page properly", async () => {
-    await act(async () => {
-      const loadAllQuizzes = jest.fn().mockResolvedValue(testQuizzes);
-      const { getByText, container } = render(<MemoryRouter><HomePage /></MemoryRouter>);
-      const linkElement = getByText(/List of Quizzes/i);
-      expect(linkElement).toBeInTheDocument();
-      expect(container).toMatchSnapshot();
+    const mockLoadAllQuizzes = jest.fn().mockResolvedValue([testQuizzes[0]]);
+    const { getByText, container } = render(<MemoryRouter><HomePage loadAllQuizzes={mockLoadAllQuizzes} /></MemoryRouter>);
+    const linkElement = getByText(/List of Quizzes/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
 
-      await waitForElementToBeRemoved(() => getByText('Loading...'))
-    })
-
+    await waitForElementToBeRemoved(() => getByText('Loading...'))
   });
 
 
   test("loads quizzes properly", async () => {
-    await act(async () => {
-      const loadAllQuizzes = jest.fn().mockResolvedValue(testQuizzes);
-      const { getByText, container } = render(<MemoryRouter><HomePage /></MemoryRouter>);
-      const linkElement = getByText(/List of Quizzes/i);
-      expect(linkElement).toBeInTheDocument();
-      expect(container).toMatchSnapshot();
+    const mockLoadAllQuizzes = jest.fn().mockResolvedValue(testQuizzes);
+    const { getByText, container } = render(<MemoryRouter><HomePage loadAllQuizzes={mockLoadAllQuizzes} /></MemoryRouter>);
+    const linkElement = getByText(/List of Quizzes/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
 
-      await waitFor(() => getByText(testQuizzes[0].title));
-      getByText(testQuizzes[1].title);
-    })
-
+    await waitFor(() => getByText(testQuizzes[0].title));
+    getByText(testQuizzes[1].title);
   });
 })
