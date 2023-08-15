a57b096fbcef0c07d04a2d905e0220f8cf67f5af
Raymond Price <darthbob88@users.noreply.github.com>
2023-08-15 14:59:55 -0700

    Adding comment for deactivated test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,8 @@ import { act, render } from "@testing-library/react";
 import App from "./App";
 import { MemoryRouter } from "react-router-dom";
 
+// TODO: Deactivated because it has the same problem as the Homepage test, relying on Firebase.
+// I expect the best solution would be to put the quiz service in a module we can jest.spyOn but that can wait.
 xtest("renders the app properly", async () => {
   await act(async () => {
     const { getByText, container } = render(<MemoryRouter><App /></MemoryRouter>);
