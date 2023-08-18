2caf709f61b029370d727dfa4d681eec91084c60
Raymond Price <darthbob88@users.noreply.github.com>
2023-08-17 17:15:54 -0700

    Messing with comments.

--- src/model/QuizService.ts
+++ src/model/QuizService.ts
@@ -1,12 +1,11 @@
 import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
 import { databaseRef } from "../firebase";
 import { Quiz } from "./Quiz"
+
 /**
- * Service for loading quizzes from whatever DB I decide to use.
+ * Loads the full list of quizzes.
+ * @returns Promise<Quiz[]> containing a full list of quizzes.
  */
-
-// TODO: Replace this with actual async loading from Firebase or wherever
-
 export const loadAllQuizzes = async (): Promise<Quiz[]> => {
     const quizCollection = collection(databaseRef, 'quizzes');
     const quizSnapshot = await getDocs(quizCollection);
@@ -14,10 +13,13 @@ export const loadAllQuizzes = async (): Promise<Quiz[]> => {
     return quizList;
 }
 
-/** Loading single quiz by their slug.
+/**
+ * Loading single quiz by their slug.
  * TODO: Should this take the title, and convert it to a slug inside the function?
+ * @param slug: string 
+ * @returns Promise<Quiz>
  */
-export const loadQuiz = async (slug: string) => {
+export const loadQuiz = async (slug: string): Promise<Quiz> => {
     const quizDocRef = doc(databaseRef, `quizzes/${slug}`);
     const selectedQuiz = await getDoc(quizDocRef);
     if (selectedQuiz != null) {
@@ -26,7 +28,10 @@ export const loadQuiz = async (slug: string) => {
         throw new Error(`Failed to load quiz ${slug}`);
     }
 }
-
+/**
+ * Saves a quiz to the Firebase store.
+ * @param newQuiz 
+ */
 export const saveQuiz = async (newQuiz: Quiz) => {
     const quizDocRef = doc(databaseRef, `quizzes/${newQuiz.slug}`);
     await setDoc(quizDocRef, newQuiz);
