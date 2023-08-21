05c0a113e39d8015ca8e03b704a4d1e6d8dd926c
Raymond Price <darthbob88@users.noreply.github.com>
2023-08-21 12:10:39 -0700

    Fixed App test by spying on the problematic function

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import { act, render } from "@testing-library/react";
 import App from "./App";
 import { MemoryRouter } from "react-router-dom";
+import * as QuizService from "./model/QuizService";
+import { testQuizzes } from "./model/Quiz";
 
 // TODO: Deactivated because it has the same problem as the Homepage test, relying on Firebase.
 // I expect the best solution would be to put the quiz service in a module we can jest.spyOn but that can wait.
-xtest("renders the app properly", async () => {
+test("renders the app properly", async () => {
+  jest.spyOn(QuizService, "loadAllQuizzes").mockResolvedValue([testQuizzes[0]]);
+
   await act(async () => {
     const { getByText, container } = render(<MemoryRouter><App /></MemoryRouter>);
     const linkElement = getByText(/Raymond's Quiz App/i);
